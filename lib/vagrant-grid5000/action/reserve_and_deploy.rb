05c79bd52621963409f1ef4b19a4002bd51cada7
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2016-02-25 19:37:16 +0100

    reindent properly

--- lib/vagrant-grid5000/action/reserve_and_deploy.rb
+++ lib/vagrant-grid5000/action/reserve_and_deploy.rb
@@ -19,21 +19,21 @@ module VagrantPlugins
           cfg = env[:machine].provider_config
           walltime = cfg.walltime
           if walltime.nil? # slightly broken: what if the local time doesn't switch DST at the same time as Europe/Paris?
-             if Time::now.dst?
-               target = '18:55:00 CEST'
-             else
-               target = '18:55:00 CET'
-             end
-             walltime = (Time::parse(target) - Time::now).to_i
-             walltime += 86400 if walltime < 0 # if we are after the deadline, target the next day
-             walltime = format("%02d:%02d:%02d", walltime / (60*60), walltime / 60 % 60, walltime % 60)
+            if Time::now.dst?
+              target = '18:55:00 CEST'
+            else
+              target = '18:55:00 CET'
+            end
+            walltime = (Time::parse(target) - Time::now).to_i
+            walltime += 86400 if walltime < 0 # if we are after the deadline, target the next day
+            walltime = format("%02d:%02d:%02d", walltime / (60*60), walltime / 60 % 60, walltime % 60)
           end
           if ENV['VAGRANT_DEBUG'] == 'REUSE_JOB'
-						job = env[:g5k].get_my_jobs(cfg.site).first
+            job = env[:g5k].get_my_jobs(cfg.site).first
           else
-						# hack: create a temporary file that holds the Vagrant public key, so that ruby-cute is happy.
-						f = `mktemp /tmp/vagrant-grid5000-public-key.XXXXXX.pub`.chomp
-						File::open(f, 'w') { |fd| fd.print VAGRANT_INSECURE_PUBLIC_KEY }
+            # hack: create a temporary file that holds the Vagrant public key, so that ruby-cute is happy.
+            f = `mktemp /tmp/vagrant-grid5000-public-key.XXXXXX.pub`.chomp
+            File::open(f, 'w') { |fd| fd.print VAGRANT_INSECURE_PUBLIC_KEY }
             params = { :site => cfg.site, :walltime => walltime, :properties => cfg.properties, :env => cfg.env, :keys => f.gsub('.pub', ''), :name => "vagrant-g5k" }
             @logger.info("Initiating reservation and deployment with #{params.inspect}")
             job = env[:g5k].reserve(params)
